ebfa1fa4a79fc832334589d87d4b8ba57539d7af
kIcKkILL <kikyouer@gmail.com>
2014-02-27 21:26:44 +0800

    improved benchmark

--- benchmark.cpp
+++ benchmark.cpp
@@ -6,27 +6,35 @@ using namespace std;
 
 int main() {
 	NaiveDB db("benchmark.xml");
+	cout << "Insertion:" << endl;
 	clock_t before_insert = clock();
-	for (long i = 0; i != 1000000; ++i) {
-		vector<DBData> line;
-		DBData dbd(DBType::INT64);
-		dbd.int64 = i;
-		line.push_back(dbd);
-		db.insert("bmtable",line);
+	for (long ii = 0; ii != 1000000; ii += 50000) {
+		long i;
+		for (i = ii; i != ii + 50000; ++i) {
+			vector<DBData> line;
+			DBData dbd(DBType::INT64);
+			dbd.int64 = i;
+			line.push_back(dbd);
+			db.insert("bmtable",line);
+		}
+		clock_t after_insert = clock();
+		cout << "  " << i << " completed after " << 
+			((double)(after_insert - before_insert)/CLOCKS_PER_SEC*1000) << "ms" << endl;
 	}
-	clock_t after_insert = clock();
-	cout << "Insertion completed after " << 
-		((after_insert - before_insert)/CLOCKS_PER_SEC) << "s" << endl;
 
+	cout << "Query:" << endl;
 	before_insert = clock();
-	for (long i = 1; i <= 1000000; ++i) {
-		DBData dbd(DBType::INT64);
-		dbd.int64 = i;
-		db.query("bmtable","id",dbd);
+	for (long ii = 1; ii <= 1000000; ii += 50000) {
+		long i;
+		for (i = ii; i != ii + 50000; ++i) {
+			DBData dbd(DBType::INT64);
+			dbd.int64 = i;
+			db.query("bmtable","id",dbd);
+		}
+		clock_t after_insert = clock();
+		cout << "  " << i - 1 << " completed after " << 
+			((double)(after_insert - before_insert)/CLOCKS_PER_SEC*1000) << "ms" << endl;
 	}
-	after_insert = clock();
-	cout << "Query completed after " << 
-		((after_insert - before_insert)/CLOCKS_PER_SEC) << "s" << endl;
 
 	before_insert = clock();
 	DBData dbd_f(DBType::INT64);
@@ -34,8 +42,8 @@ int main() {
 	DBData dbd_l(DBType::INT64);
 	dbd_l.int64 = 1000000;
 	db.rangeQuery("bmtable","id",dbd_f,dbd_l);
-	after_insert = clock();
+	clock_t after_insert = clock();
 	cout << "Range query completed after " << 
-		((after_insert - before_insert)/CLOCKS_PER_SEC) << "s" << endl;
+		((double)(after_insert - before_insert)/CLOCKS_PER_SEC*1000) << "ms" << endl;
 	return 0;
 }
